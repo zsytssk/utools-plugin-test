ac2460b7d344d8da6721e483b0d497e2d479ee22
shiyangzhang <shiyangzhang@whale.im>
2023-05-30 20:15:32 +0800

    [save] [test1]

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,5 +1,3 @@
-import eslint from '@rollup/plugin-eslint';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import commonjsExternals from 'vite-plugin-commonjs-externals';
 
@@ -10,7 +8,6 @@ export default defineConfig({
         exclude: externals as any[],
     },
     plugins: [
-        react(),
         commonjsExternals({
             externals,
         }),
@@ -18,13 +15,10 @@ export default defineConfig({
     build: {
         outDir: './dist',
         rollupOptions: {
-            input: {
-                index: './index.html',
-                preload: './preload/main.ts',
-            },
+            input: './preload/main.ts',
             output: {
                 strict: false,
-                entryFileNames: '[name].js',
+                entryFileNames: 'preload.js',
                 dir: 'dist',
             },
         },
